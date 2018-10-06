ce8250069d5d339fbff4b96bc66cd805216f3fd4
T-107 <T-107@PC199>
2018-10-05 21:40:53 -0500

    Rutas con diagonales

---
+++ src/main/java/unitec/primavera/ControladorMensajitos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.primavera;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author T-107
+ */
+@RestController
+@RequestMapping("/api")
+public class ControladorMensajitos {
+    @Autowired RepositorioMensajes mensa;
+    // Mi primer Hola Mundo desde REST API
+    @GetMapping(path="/hola")
+    public String hola()
+    {
+        return "Hola Mundo";
+    }
+    
+    // Buscamos todos
+    @GetMapping(path="/mensaje")
+    public List<Mensajito> todos()
+    {
+        return mensa.findAll();
+    }
+    
+    // Buacamos por id
+    @GetMapping(path="/mensaje/{id}")
+    public Mensajito buscarPorId(@PathVariable String id)
+    {
+        return mensa.findById(id).get();
+    }
+}
--- src/main/java/unitec/primavera/PrimaveraApplication.java
+++ src/main/java/unitec/primavera/PrimaveraApplication.java
@@ -24,7 +24,7 @@ public class PrimaveraApplication implements CommandLineRunner {
         // repoPagos.servicioPagar();
 
         // Guarda un mensaje
-        // repoMensa.save(new Mensajito("SQL vs PHP", "¿Quién ganará?", LocalDate.now()));
+        // repoMensa.save(new Mensajito("PHP vs SQL", "¿Quién ganará?", LocalDate.now()));
         
         /*
         // Buscamos todos los mensajes
@@ -38,11 +38,19 @@ public class PrimaveraApplication implements CommandLineRunner {
         System.out.println(repoMensa.findById("5bb422b4c35d8012e45e0e1d").get());
         */
         
-        // Buscar por título
+        /*
+        // Actualizar
         repoMensa.save(new Mensajito("malo", "yo", "topoyiya", LocalDate.now()));
+        */
+        
+        /*
+        // Buscar por título
         System.out.println(repoMensa.findByTitulo("yo"));
+        */
         
+        /*
+        // Borrar
         repoMensa.delete(new Mensajito("malo"));
-        
+        */
     }
 }
